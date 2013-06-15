33113899571693b428b37460bf36c2eee65dac8d
SOMTD <matsuda-so@kayac.com>
2013-06-16 00:10:08 +0900

    fix position.

--- SWScrollView/SWViewController.m
+++ SWScrollView/SWViewController.m
@@ -21,10 +21,17 @@
     [super viewDidLoad];
     
     CATransform3D transform = CATransform3DIdentity;
-    transform.m34 = 1.0f / -500.0f;
-    transform = CATransform3DRotate(transform, 60.0f * M_PI / 180.0f, 1.0f, 0.0f, 0.0f);
-    
+    //z distance
+    float distance = [[UIScreen mainScreen] bounds].size.height;
+    float ratio    = [[UIScreen mainScreen] bounds].size.height/[[UIScreen mainScreen] bounds].size.height;
+    transform.m34 = - ratio / distance;;
+    transform = CATransform3DRotate(transform, 60.0f * M_PI / 180.0f, 1.f, 0.0f, 0.0f);
     _textView.layer.transform = transform;
+    _textView.layer.zPosition = distance * ratio;
+    _textView.layer.position = CGPointMake(
+                                           [[UIScreen mainScreen] bounds].size.width/2,
+                                           [[UIScreen mainScreen] bounds].size.height/3);
+
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
